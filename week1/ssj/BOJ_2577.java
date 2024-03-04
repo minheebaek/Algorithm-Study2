c12795a865f5fd56f44ed51c5318c98bb20684e2
zzoo0123 <0123suh@gmail.com>
2024-03-04 22:53:46 +0900

    Auto stash before rebase of "origin/main"

---
+++ week1/ssj/BOJ_2577.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BOJ_2577 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int value = (sc.nextInt() * sc.nextInt() * sc.nextInt());
+        String str = Integer.toString(value);
+        sc.close();
+
+        // 0~9까지 숫자 개수 저장
+        int[] cnt = new int[10];
+
+        for (int j = 0; j < str.length(); j++) {
+            // 문자 -> 정수
+            cnt[str.charAt(j) - '0']++;
+        }
+
+        for (int i = 0; i < cnt.length; i++) {
+            System.out.println(cnt[i]);
+        }
+    }
+}
