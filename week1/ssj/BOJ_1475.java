a42583247a60c165a39bcaf29c9ba2af42b0f107
zzoo0123 <0123suh@gmail.com>
2024-03-05 23:35:26 +0900

    Auto stash before rebase of "origin/main"

---
+++ week1/ssj/BOJ_1475.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10];
+
+        while (N > 0) {
+            arr[N % 10]++;
+            N /= 10;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 10; i++) {
+            if (i == 6 || i ==9)
+                continue;
+
+            answer = Math.max(answer, arr[i]);
+        }
+
+        answer = Math.max(answer, (arr[6] + arr[9] + 1) / 2);
+        System.out.println(answer);
+    }
+}
