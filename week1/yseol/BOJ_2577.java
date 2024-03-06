92335ef2863d3a43117b555842747b0db10e57e7
seoseo17 <eyyss16@gmail.com>
2024-03-06 20:57:49 +0900

    add: BOJ_1475(방번호)

--- week1/yseol/BOJ_2577.java
+++ week1/yseol/BOJ_2577.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,23 +9,44 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
 
         Scanner scanner = new Scanner(System.in);
-        int n1 = scanner.nextInt();
-        int n2 = scanner.nextInt();
-        int n3 = scanner.nextInt();
+        int num = scanner.nextInt();
 
-        //System.out.printf("%d,%d,%d \n", n1,n2,n3);
+        int count = 0;
+        int[] nums = new int[10];
 
-        int num = n1*n2*n3;
-        int[] count = new int[10];
-
-        while (num>0){
-            int d = num % 10;
-            num = num / 10;
-            count[d]++;
+        while(num>0){
+            int d = num%10;
+            if (d == 9) d=6;
+            num = num/10;
+            nums[d]++;
+        }
+/** 반례)666 -> 원래 코드 1 출력 되어서 if문으로 홀수일 때 처리*/
+        if (nums[6] >1){
+            if (nums[6]%2 == 0){
+                nums[6] = nums[6]/2;
+            }else {
+                nums[6] = nums[6]/2+1;
+            }
         }
 
-        for (int i: count){
-            System.out.println(i);
+//        for (int i = 0; i < 10; i++) {
+//            System.out.printf("%d : %d\n",i,nums[i]);
+//        }
+        while (true){
+            int sum = Arrays.stream(nums).sum();
+            if (sum == 0) break;
+
+            for (int i = 0; i < 10; i++) {
+                if(nums[i] == 0) continue;
+                nums[i]--;
+            }
+            count++;
+
         }
+//        for (int i = 0; i < 10; i++) {
+//            System.out.printf("%d : %d\n",i,nums[i]);
+//        }
+        System.out.println(count);
+
     }
 }
