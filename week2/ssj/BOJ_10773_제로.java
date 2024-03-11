f102afdb2343b5e88c57206ec1a7713050852a88
zzoo0123 <0123suh@gmail.com>
2024-03-11 23:37:06 +0900

    ssj : BOJ 10773 제로

---
+++ week2/ssj/BOJ_10773_제로.java
@@ -0,0 +1,31 @@
+package barkingDog.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ_10773_제로 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < k; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                stack.pop();
+            } else {
+                stack.push(n);
+            }
+        }
+
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        System.out.println(sum);
+        br.close();
+    }
+}
