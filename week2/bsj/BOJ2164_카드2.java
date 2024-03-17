a2d8ec69d0383ca8a8d87cb3cb771b04b28840f7
bsjin1122 <kitty7579@hanmail.net>
2024-03-17 23:22:33 +0900

    BSJ : BOJ 2164 카드2

---
+++ week2/bsj/BOJ2164_카드2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ2164_카드2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=N; i++)
+            queue.offer(i);
+
+        while(queue.size()>1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
