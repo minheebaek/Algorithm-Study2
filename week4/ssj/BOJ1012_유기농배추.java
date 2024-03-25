1a76d553483a9c162b828f98a5e89fcee37bab63
zzoo0123 <0123suh@gmail.com>
2024-03-25 23:46:37 +0900

    서승주 : BOJ 1012 유기농배추

---
+++ week4/ssj/BOJ1012_유기농배추.java
@@ -0,0 +1,57 @@
+package barkingDog.bfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1012_유기농배추 {
+    static int n, m, cnt;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            arr = new int[n][m];
+            visited = new boolean[n][m];
+            cnt = 0;
+
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                arr[y][x] = 1;
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (!visited[i][j] && arr[i][j] == 1) {
+                        dfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+
+    static void dfs(int i, int j) {
+        visited[i][j] = true;
+        for (int k = 0; k < 4; k++) {
+            int x = i + dx[k];
+            int y = j + dy[k];
+            if (x >= 0 && x < n && y >= 0 && y < m && !visited[x][y] && arr[x][y] == 1) {
+                dfs(x, y);
+            }
+        }
+    }
+}
