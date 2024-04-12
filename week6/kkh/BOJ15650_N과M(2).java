c3853882d34605c9d0f2ce1c6224326a0aa3b1b9
Kimkyunghoon1117 <hoon-1117@naver.com>
2024-04-12 23:59:20 +0900

    김경훈 : BOJ 15650 N과 M (2)

--- week6/kkh/BOJ15650_N과M(2).java
+++ week6/kkh/BOJ15650_N과M(2).java
@@ -44,3 +44,29 @@ public class Main {
 		}
 	}
 }
+
+// 숏코딩
+import java.util.Scanner;
+
+public class Main {
+	static int n,m,arr[];
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	m=sc.nextInt();
+	arr=new int [n];
+	dfs(0,0);
+}
+	static void dfs(int start, int cnt) {
+		if(cnt==m) {
+			for(int i=0;i<m;i++) {
+				System.out.print(arr[i]+" ");
+			}System.out.println();
+		}
+		for(int i=start;i<n;i++) {
+			arr[cnt]=i+1;
+			dfs(i+1,cnt+1);
+			
+		}
+	}
+}
